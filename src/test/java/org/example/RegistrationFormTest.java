bbb6f257d386ba1f4908f4b554e0d46d4615f015
Calandarov <calandarov@icloud.com>
2023-03-24 00:05:19 +0300

    add class for registration form

---
+++ src/test/java/org/example/RegistrationFormTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RegistrationFormTest {
+    WebDriver driver = new ChromeDriver();
+
+
+}
