cdc83ed753c5a712ace64d4cfbce276942bf2d8a
Calandarov <calandarov@icloud.com>
2023-03-24 23:45:55 +0300

    replaced JUnit with TestNG, added 2 classes

---
+++ src/main/java/common/CommonActions.java
@@ -0,0 +1,15 @@
+package common;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class CommonActions {
+    public static void createDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        System.setProperty("webdriver.chrome.driver", "/Users/dmitrijkalandarov/documents/programming/chromedriver/chromedriver");
+        WebDriver driver = new ChromeDriver(options);
+    }
+}
