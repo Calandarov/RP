250dc3ccf068d37745bcde6404c373100103230a
Kalandarov D <calandarov@icloud.com>
2023-03-27 19:44:31 +0300

    staff

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,19 +1,29 @@
-package common;
+package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class CommonActions {
-    public static void createDriver() {
+public class BasePage {
+    protected WebDriver driver;
+
+    public BasePage() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
 //        System.setProperty("webdriver.chrome.driver", "/Users/dmitrijkalandarov/documents/programming/chromedriver/chromedriver");
         System.setProperty("webdriver.chrome.driver", "C://chromedriver/chromedriver");
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
+    }
+
+    public void openPage(String url) {
+        driver.get(url);
+    }
+
+    public void closePage() {
+        driver.close();
     }
 }
---
+++ src/main/java/pages/RegistrationFormPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+public class RegistrationFormPage extends BasePage {
+    public RegistrationFormPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Test
+
+}
