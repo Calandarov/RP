22c3ac8142c9fbecf995901c07ef8b8883b7dea0
Kalandarov D <calandarov@icloud.com>
2023-03-29 15:00:51 +0300

    staff

--- src/test/java/driver/Driver.java
+++ src/test/java/driver/Driver.java
@@ -5,13 +5,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Driver {
-    static WebDriver driver;
+    public static WebDriver driver;
+    ChromeOptions options = new ChromeOptions();
+    public ChromeOptions setOptions() {
+        options.addArguments("--remote-allow-origins=*");
 
-    public Driver(ChromeOptions options) {
+        return options;
+    }
+    public Driver() {
+//        System.setProperty("webdriver.chrome.driver", "/Users/dmitrijkalandarov/documents/programming/chromedriver/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C://chromedriver/chromedriver");
         driver = new ChromeDriver(options);
     }
 
-    public static void clearCoockies() {
+    public void clearCoockies() {
         driver.manage().deleteAllCookies();
     }
 }
---
+++ src/test/java/pages/home/HomePageTest.java
@@ -0,0 +1,14 @@
+package pages.home;
+
+
+import driver.Driver;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class HomePageTest {
+
+    Driver driver = new Driver();
+
+    driver.get("https://google.com");
+
+}
